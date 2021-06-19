0d1321cd6433c0b97969e2dde5a2c846fb2dd7e3
israelias <israelias.js@gmail.com>
2021-06-19 23:37:41 +0800

    Add test routes for tutors, students and to-be-joined n:m endpoints

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,11 +1,39 @@
 import express from 'express';
-
+import * as db from '../models/index';
 import { validate } from 'express-validation';
 
 import * as apiController from '../controllers/api.controller';
 import * as apiValidator from '../controllers/api.validator';
 
 const router = express.Router();
+
+// GET all tutors
+router.get('/tutors', async (req, res) => {
+  db.Tutor.findAll({}).then(function (result) {
+    res.json(result);
+  });
+});
+
+// GET all students
+router.get('/students', async (req, res) => {
+  db.Student.findAll({}).then(function (result) {
+    res.json(result);
+  });
+});
+
+// GET all common tutorstudents
+router.get('/commonstudents', async (req, res) => {
+  db.TutorStudent.findAll({}).then(function (result) {
+    res.json(result);
+  });
+});
+
+// GET all common tutorstudents
+router.get('/retrievenotifications', async (req, res) => {
+  db.StudentNotification.findAll({}).then(function (result) {
+    res.json(result);
+  });
+});
 
 // api/register
 router.post(
