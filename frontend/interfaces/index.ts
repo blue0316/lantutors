5d11ff5f6d2a13928935c6153fdc517a0cf1d595
israelias <israelias.js@gmail.com>
2021-06-21 17:35:13 +0800

    Define typescript interfaces based on backend api

--- frontend/interfaces/index.ts
+++ frontend/interfaces/index.ts
@@ -5,6 +5,59 @@
 // import { User } from 'path/to/interfaces';
 
 export type User = {
-  id: number
-  name: string
+  id: number;
+  name: string;
+};
+
+export interface Student {
+  id?: number;
+  username?: string;
+  email: string;
+  suspended?: boolean;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface Students {
+  students: Student['email'][];
+}
+
+export interface Tutor {
+  id?: number;
+  username?: string;
+  email: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface CommonStudent {
+  tutor: string;
+  name: string;
+  students: Student['email'][];
+}
+
+export interface CommonStudents {
+  tutorName: string;
+  students: {
+    id: Student['id'];
+    studentName: Student['username'];
+  }[];
+}
+export interface Notification {
+  id: number;
+  studentName: Student['username'];
+  tutorName: Tutor['username'];
+  title: string;
+  message: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface Notifications {
+  title: string;
+
+  tutorName: Tutor['username'];
+
+  message: string;
+  recipients: [studentName: Student['username']];
 }
