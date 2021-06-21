e06153581ea168609de788836ec0f960b9991ad0
israelias <israelias.js@gmail.com>
2021-06-21 17:37:40 +0800

    Define boiletplaye nextjs layout with conditional constants

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,41 +1,60 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+
+
+import React, { ReactNode } from 'react';
+
+import Head from 'next/head';
+import { CssBaseline } from '@material-ui/core';
+
+// import Footer from './Footer';
+import Header from './Header';
+import { Tutor } from '../interfaces';
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+  children?: ReactNode;
+  title?: string;
+  tutors: Tutor[];
+  about?: string;
+};
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
+const Layout = ({
+  children,
+  title = "Ring's Listings",
+  about = 'Meaningful Classifieds',
+  tutors,
+}: Props) => (
+  <>
+    <CssBaseline />
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="viewport"
+        content="initial-scale=1.0, width=device-width"
+      />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+    <>
+      <>
+        <Header tutors={tutors} />
+        {/* <nav>
+          <Link href="/">
+            <a>Home</a>
+          </Link>{' '}
+          |{' '}
+          <Link href="/about">
+            <a>About</a>
+          </Link>{' '}
+          |{' '}
+          <Link href="/users">
+            <a>Users List</a>
+          </Link>{' '}
+          | <a href="/api/users">Users API</a>
+        </nav> */}
+      </>
+      {children}
+      {/* <Footer title={title} description={about} /> */}
+    </>
+  </>
+);
 
-export default Layout
+export default Layout;
+
