cd55f258de4ffef377e86c1d60e572397826ab7d
israelias <israelias.js@gmail.com>
2021-06-26 22:20:43 +0800

    Replace item component with refined card lists

--- frontend/components/CommonCard.tsx
+++ frontend/components/CommonCard.tsx
@@ -1,39 +1,60 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
-import Button from '@material-ui/core/Button';
+import { Theme } from '@material-ui/core/styles';
+import {
+  Avatar,
+  Box,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Typography,
+  Button,
+  AvatarGroup,
+  SvgIcon,
+} from '@material-ui/core';
 
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/styles';
 
-import Grid from '@material-ui/core/Grid';
+import CommonList from './CommonList';
 
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import {
+  initialize,
+  capitalize,
+  randomColor,
+} from '../utils/initialize';
 
-import List from './List';
-import ListItem from './ListItem';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import dayjs from 'dayjs';
+dayjs.extend(relativeTime);
 
-// import { useUserContext } from '../context/user.context';
-// import { useDataHandler } from '../context/data.context';
-import { CommonStudents } from '../interfaces';
+import {
+  Home as HomeIcon,
+  Plus as AddIcon,
+  AtSign as NotificationsIcon,
+  User as UserIcon,
+  UserPlus as UserPlusIcon,
+  Users as Students,
+} from 'react-feather';
 
-const useStyles = makeStyles((theme) => ({
+import { useTutor } from '../context/tutor.context';
+
+const useStyles = makeStyles((theme: Theme) => ({
   icon: {
-    marginRight: theme.spacing(2),
+    marginRight: '16px',
   },
   heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
+    // backgroundColor: theme.palette.background.paper,
+    padding: '48px 0px 36px',
   },
   heroButtons: {
-    marginTop: theme.spacing(4),
+    marginTop: '30px',
   },
   cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
+    paddingTop: '48px',
+    paddingBottom: '48px',
   },
   card: {
     height: '100%',
@@ -47,84 +68,142 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
+    // backgroundColor: theme.palette.background.paper,
+    padding: '48px',
   },
 }));
 
-type Props = {
-  commonStudents: CommonStudents[];
-};
-const CommonCard = ({ commonStudents }: Props) => {
+const CommonCard = ({
+  commonStudent,
+}: {
+  commonStudent: CommonStudentsApi;
+}) => {
   const classes = useStyles();
   const router = useRouter();
 
-  // const { userId } = useUserContext();
-  // const { setEditing, setId, setHeading } = useDataHandler();
+  const { email } = useTutor();
   return (
-    <Container className={classes.cardGrid} maxWidth="lg">
-      <Grid container spacing={4}>
-        {commonStudents.map((common) => (
+    <Card
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+      }}
+    >
+      <CardContent>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            pb: 3,
+          }}
+        >
+          <Typography
+            align="center"
+            color="textPrimary"
+            gutterBottom
+            variant="h4"
+          >
+            Tutor {initialize(commonStudent.tutor)}
+          </Typography>
+
+          <Avatar
+            style={{
+              backgroundColor: randomColor(),
+            }}
+          >
+            {/* {capitalize(commonStudent.student)} */}
+          </Avatar>
+        </Box>
+        <CommonList students={commonStudent.students} />
+
+        {/* <Typography align="left" color="textPrimary" variant="body2">
+          {notification.message}
+        </Typography> */}
+      </CardContent>
+      <Box sx={{ flexGrow: 1 }} />
+      <Divider />
+      <Box sx={{ p: 2 }}>
+        <Grid
+          container
+          spacing={2}
+          sx={{ justifyContent: 'space-between' }}
+        >
           <Grid
             item
-            key={common.tutorName}
-            xs={6}
-            sm={4}
-            md={3}
-            lg={3}
+            sx={{
+              alignItems: 'center',
+              display: 'flex',
+            }}
           >
-            <Card className={classes.card}>
-              <CardContent className={classes.cardContent}>
-                <Typography gutterBottom variant="h5" component="h4">
-                  {common.tutorName}
-                </Typography>
-                <Typography variant="subtitle1" color="textSecondary">
-                  <List students={common.students} />
-                  {/* <Typography variant="overline">₱</Typography>{' '} */}
-                  {/* {common.price} */}
-                </Typography>
-                <Typography variant="body2">
-                  {/* {common.description} */}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button
-                  size="small"
-                  color="primary"
-                  onClick={() =>
-                    router.push({
-                      pathname: `/commonstudents/${encodeURIComponent(
-                        common.tutorName
-                      )}`,
-                    })
-                  }
-                >
-                  View
-                </Button>
-                {true && (
-                  <Button
-                    size="small"
-                    color="primary"
-                    onClick={() => {
-                      // setEditing(true);
-                      // setId(listing.id);
-                      // setHeading(`Editing ${listing.title}`);
-                      router.push({
-                        pathname: `/edit/${encodeURIComponent(
-                          common.tutorName
-                        )}`,
-                      });
-                    }}
-                  >
-                    Edit
-                  </Button>
-                )}
-              </CardActions>
-            </Card>
+            {/* <AccessTimeIcon color="action" /> */}
+            <Typography
+              color="textSecondary"
+              display="inline"
+              sx={{ pl: 1 }}
+              variant="body2"
+            >
+              {commonStudent.students.length} students assigned to
+              @tutor{initialize(commonStudent.tutor)}
+            </Typography>
           </Grid>
-        ))}
-      </Grid>
-    </Container>
+          <Grid
+            item
+            sx={{
+              alignItems: 'center',
+              display: 'flex',
+            }}
+          >
+            {email && (
+              <Button
+                size="small"
+                color="primary"
+                onClick={() => {
+                  router.push({
+                    pathname: `/tutorstudents/${encodeURIComponent(
+                      commonStudent.tutor && commonStudent.tutor
+                    )}`,
+                  });
+                }}
+              >
+                Send a messsage to your students
+              </Button>
+            )}
+
+            <NotificationsIcon />
+            <Typography
+              color="textSecondary"
+              display="inline"
+              sx={{ pl: 1 }}
+              variant="body2"
+            >
+              View
+            </Typography>
+            {/* <Button
+              size="small"
+              color="primary"
+              onClick={() => {
+                router.push({
+                  pathname: `/tutorstudents/${encodeURIComponent(
+                    commonStudent.tutor
+                  )}`,
+                });
+              }}
+            >
+              Send a messsage to your students
+            </Button> */}
+            <Link
+              href={`/tutorstudents/${encodeURIComponent(
+                commonStudent.tutor
+              )}`}
+            >
+              <a>View</a>
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Card>
   );
 };
 
---
+++ frontend/components/CommonList.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { createStyles, makeStyles } from '@material-ui/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Checkbox from '@material-ui/core/Checkbox';
+import Avatar from '@material-ui/core/Avatar';
+
+import {
+  initialize,
+  capitalize,
+  randomColor,
+  firstInitial,
+} from '../utils/initialize';
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      maxWidth: 360,
+      // backgroundColor: theme.palette.background.paper,
+    },
+  })
+);
+
+const CommonList = ({
+  students,
+}: {
+  students: Student['email'][];
+}) => {
+  const classes = useStyles();
+  const router = useRouter();
+  const [checked, setChecked] = React.useState([1]);
+
+  const handleToggle = (value: number) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  return (
+    <List dense className={classes.root}>
+      {students.map((student) => {
+        const localId = `common-${initialize(student)}`;
+        return (
+          <ListItem
+            key={student}
+            button
+            onClick={() => {
+              router.push({
+                pathname: `/students/${encodeURIComponent(student)}`,
+              });
+            }}
+          >
+            <ListItemAvatar>
+              <Avatar
+                style={{
+                  backgroundColor: randomColor(),
+                }}
+              >
+                {capitalize(student)}
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText id={student} primary={student} />
+            <ListItemSecondaryAction>
+              <Checkbox
+                edge="end"
+                onChange={handleToggle(student)}
+                checked={checked.indexOf(student) !== -1}
+                inputProps={{ 'aria-labelledby': localId }}
+              />
+            </ListItemSecondaryAction>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+};
+
+export default CommonList;
